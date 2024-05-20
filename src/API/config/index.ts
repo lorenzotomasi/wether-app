62ddeaf7a59ede6206d3779a322ba59d91382930
Lorenzo Tomasi <lorenzo1208tomasi@gmail.com>
2024-05-20 09:06:40 +0200

    Fix endpoint

--- src/API/config/index.ts
+++ src/API/config/index.ts
@@ -1,4 +1,4 @@
-const WETHER_API_BASE_URL = `http://api.weatherstack.com...` as const
+const WETHER_API_BASE_URL = `https://api.weatherstack.com` as const
 const WETHER_API_KEY = `b3c5b52569492b97a7678ed2e9f1ecd8` as const
 
 export { WETHER_API_BASE_URL, WETHER_API_KEY }
