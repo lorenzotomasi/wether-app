2d97b46c7782cecdb76e71de05211e8d7a3fccad
Lorenzo Tomasi <lorenzo1208tomasi@gmail.com>
2024-05-20 09:07:14 +0200

    Remove comments

--- src/API/current/index.ts
+++ src/API/current/index.ts
@@ -14,45 +14,6 @@ function getCurrentWether(location: WetherLocationInputType, unit: UserPreferenc
       }
       return res.json().then((r: WetherInformationBE) => wetherInformationBEToWetherInformation(r))
     })
-    // .catch(() =>
-    //   wetherInformationBEToWetherInformation({
-    //     "request": {
-    //         "type": "City",
-    //         "query": "New York, United States of America",
-    //         "language": "en",
-    //         "unit": "m"
-    //     },
-    //     "location": {
-    //         "name": "New York",
-    //         "country": "United States of America",
-    //         "region": "New York",
-    //         "lat": "40.714",
-    //         "lon": "-74.006",
-    //         "timezone_id": "America/New_York",
-    //         "localtime": "2024-05-19 14:18",
-    //         "localtime_epoch": 1716128280,
-    //         "utc_offset": "-4.0"
-    //     },
-    //     "current": {
-    //         "observation_time": "06:18 PM",
-    //         "temperature": 22,
-    //         "weather_code": 308,
-    //         "weather_descriptions": [
-    //             "Partly cloudy"
-    //         ],
-    //         "wind_speed": 17,
-    //         "wind_degree": 50,
-    //         "wind_dir": "NE",
-    //         "pressure": 1015,
-    //         "precip": 0,
-    //         "humidity": 53,
-    //         "cloudcover": 75,
-    //         "feelslike": 25,
-    //         "uv_index": 5,
-    //         "visibility": 16,
-    //     }
-    // })
-    // );
 }
 
 export { getCurrentWether };
